6f7511fdef435c819e88ae05c753a86beb913fbd
Mateusz Kmieciak <mkmieciak70@gmail.com>
2021-12-10 13:30:01 +0100

    Detail view of a log event

--- src/MicroLog.Lama/Client/Pages/Base/BasePage.cs
+++ src/MicroLog.Lama/Client/Pages/Base/BasePage.cs
@@ -1,5 +1,25 @@
-﻿namespace MircoLog.Lama.Client.Pages.Base;
+﻿using MicroLog.Core;
+using Microsoft.AspNetCore.Components;
+using MircoLog.Lama.Client.Components;
+using MudBlazor;
+
+namespace MircoLog.Lama.Client.Pages.Base;
 
 public abstract class BasePage : BaseComponent
 {
+    [Inject] private IDialogService DialogService { get; set; }
+
+    public void ShowLogDetails(LogEvent _log)
+    {
+        var parameters = new DialogParameters();
+        parameters.Add("Log", _log);
+        var options = new DialogOptions()
+        {
+            CloseButton = true,
+            MaxWidth = MaxWidth.Large,
+            Position = DialogPosition.Center
+        };
+
+        DialogService.Show<LogDetailsDialog>("Log Details", parameters, options);
+    }
 }
