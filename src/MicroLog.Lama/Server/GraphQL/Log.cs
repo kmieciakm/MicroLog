f8689dcd9e57c6b113a25243ed30bb45d198ed65
Mateusz Kmieciak <mkmieciak70@gmail.com>
2021-12-10 18:00:22 +0100

    GraphQL log model completed

---
+++ src/MicroLog.Lama/Server/GraphQL/Log.cs
@@ -0,0 +1,32 @@
+﻿namespace MircoLog.Lama.Server.GraphQL;
+
+public class Log
+{
+    public LogIdentity Identity { get; init; }
+    public string Message { get; set; }
+    public DateTime Timespan { get; set; }
+    public MicroLog.Core.LogLevel Level { get; set; }
+    public string LevelName { get; set; }
+    public LogException Exception { get; set; }
+    public IEnumerable<LogProperty> Properties { get; set; }
+
+    public static IEnumerable<Log> Parse(IEnumerable<ILogEvent> logs)
+    {
+        return logs.Select(log => new Log()
+        {
+            Identity = LogIdentity.Parse(log.Identity),
+            Message = log.Message,
+            Timespan = log.Timestamp,
+            Level = log.Level,
+            LevelName = Enum.GetName(typeof(MicroLog.Core.LogLevel), log.Level),
+            Exception = log.Exception,
+            Properties = log.Properties.Select(prop =>
+                new LogProperty()
+                {
+                    Name = prop.Name,
+                    Value = prop.Value,
+                }
+            )
+        });
+    }
+}
--- src/MicroLog.Lama/Server/GraphQL/Query.cs
+++ src/MicroLog.Lama/Server/GraphQL/Query.cs
@@ -8,18 +8,3 @@ public class Query
         return Log.Parse(logs);
     }
 }
-
-public class Log
-{
-    public string Message { get; set; }
-    public DateTime Timespan { get; set; }
-
-    public static IEnumerable<Log> Parse(IEnumerable<ILogEvent> logs)
-    {
-        return logs.Select(log => new Log()
-        {
-            Message = log.Message,
-            Timespan = log.Timestamp
-        });
-    }
-}
