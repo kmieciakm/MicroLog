6bf8abec7ca3d96fb17c2b41eac4963ab1444b79
Mateusz Kmieciak <mkmieciak70@gmail.com>
2021-12-17 00:00:32 +0100

    Inovke graphQl query from app

--- src/MicroLog.Lama/Client/Services/Filter.cs
+++ src/MicroLog.Lama/Client/Services/Filter.cs
@@ -1,7 +1,33 @@
-﻿namespace MircoLog.Lama.Client.Services;
+﻿using MicroLog.Core;
+using System;
+using System.Collections.Generic;
+
+namespace MircoLog.Lama.Client.Services;
 
 class Filter
 {
     public string Name { get; set; }
     public string Query { get; set; }
+}
+
+public class FilterResponse
+{
+    public Logs Logs { get; set; }
+}
+
+public class Logs
+{
+    public long? TotalCount { get; set; }
+    public List<LogItem> Items { get; set; }
+}
+
+public class LogItem
+{
+    public LogIdentity Identity { get; set; }
+    public DateTime? Timestamp { get; set; }
+    public string Message { get; set; }
+    public string Level { get; set; }
+    public string LevelName { get; set; }
+    public LogException Exception { get; set; }
+    public List<LogProperty> Properties { get; set; }
 }
